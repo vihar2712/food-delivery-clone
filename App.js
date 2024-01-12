9e9f0068b17498a64582ff10de26565384e32e7f
viharshah27 <Vihar.shah@asambhav.org.in>
2024-01-12 11:39:34 +0530

    Ep-01-inception

---
+++ App.js
@@ -0,0 +1,21 @@
+const heading = React.createElement(
+  "h1",
+  { style: { backgroundColor: "red" }, id: "heading" },
+  "Hello World from React"
+);
+console.log(heading); // it is an object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I an an H1 tag"),
+    React.createElement("h2", {}, "I an an H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I an an H1 tag"),
+    React.createElement("h2", {}, "I an an H2 tag"),
+  ]),
+]);
+// root.render(heading);
+root.render(parent);
---
+++ README.md
@@ -0,0 +1,1 @@
+#Namaste React course by Akshay Saini.
